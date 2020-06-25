9d493828d3ab4c94ff172d7d7acaa28d0388eca8
Quang Le <quangld@gearvn.com>
2020-06-25 11:50:38 +0700

    Updates

---
+++ precache-manifest.4b41e7051109c7fca9cf628fdcc27ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d93f7865d77c649d6ce581ad5a5c23",
+    "url": "/email-mkt/index.html"
+  },
+  {
+    "revision": "a933f1564efe5be280c6",
+    "url": "/email-mkt/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "a933f1564efe5be280c6",
+    "url": "/email-mkt/static/js/2.aebacca3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/email-mkt/static/js/2.aebacca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4e2b04d4b28d2bdcf5",
+    "url": "/email-mkt/static/js/main.57c25c99.chunk.js"
+  },
+  {
+    "revision": "c854d71eef3c42d3c905",
+    "url": "/email-mkt/static/js/runtime-main.d2a67a04.js"
+  }
+]);
